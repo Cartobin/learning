1e2f8a6af7e9b01d64c1e00f6daeed196b72686f
Bemoworld <blakemorgan1991@gmail.com>
2019-06-23 23:10:13 -0400

    chore: create-react-app feat: added Blake Button

---
+++ blaker/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'react-bulma-components';
+import './App.css';
+
+function App() {
+  var name = 'Start Blakeing Now';
+  function doSomething(){
+    name ='The Blakening has begun'
+  }
+  return (
+    <div className="App">
+      <Button color='info' onClick={doSomething()}>
+        {name}
+      </Button>
+    </div>
+  );
+}
+
+export default App;
